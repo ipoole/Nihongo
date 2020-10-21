76055ddec2300070f512df0ecacbc2336a3204d2
Ian(pythonista) <ipoole@icloud.com>
2020-10-21 18:49:42 +0000

    Added speach with numbers quiz

--- sentences.py
+++ sentences.py
@@ -1,8 +1,37 @@
 # coding: utf-8
-
+import speech
+from time import sleep
+import random
 from japanese import Vt, sdict
 
 # Here is a comment I added
+
+def finish_speaking():
+	# Block until speech synthesis has finished
+	while speech.is_speaking():
+		sleep(0.1)
+		
+def say_jap_wait(jap, t=0):
+	speech.say(jap, 'ja-JP', 0.4)
+	while speech.is_speaking():
+		sleep(0.2)
+	sleep(t)
+	
+def say_eng_wait(eng, t=0):
+	speech.say(eng, 'en-GB')
+	while speech.is_speaking():
+		sleep(0.15)
+	sleep(t)
+	
+		
+def print_and_say(jap_eng):
+	jap, eng = jap_eng
+	print(jap)
+	print(eng)
+	speech.say(jap, 'ja-JP')
+	finish_speaking()
+	sleep(1)
+	print()
 
 def basic_sen(subject=None, noun=None, verb=None, with_=None, *args):
 	"""
@@ -26,12 +55,12 @@ def basic_sen(subject=None, noun=None, verb=None, with_=None, *args):
 	return jap, eng
 
 def basic_sen_examples():
-	print(basic_sen('she', 'phone', 'use', None, Vt.PAST))
-	print(basic_sen('I', 'sushi', 'eat', None, Vt.PAST, Vt.PLAIN))
-	print(basic_sen('I', 'beer', 'drink', 'Tanaka-san', Vt.PAST, Vt.POLITE))
-	print(basic_sen('he', 'dog', 'lend', None))
-	print(basic_sen('she', 'class', 'finish', None, Vt.PAST))
-	print(basic_sen('I', 'bread', 'forget', None, Vt.PAST))
+	print_and_say(basic_sen('she', 'phone', 'use', None, Vt.PAST))
+	print_and_say(basic_sen('I', 'sushi', 'eat', None, Vt.PAST, Vt.PLAIN))
+	print_and_say(basic_sen('I', 'beer', 'drink', 'Tanaka-san', Vt.PAST, Vt.POLITE))
+	print_and_say(basic_sen('he', 'dog', 'lend', None))
+	print_and_say(basic_sen('she', 'class', 'finish', None, Vt.PAST))
+	print_and_say(basic_sen('I', 'bread', 'forget', None, Vt.PAST, Vt.NEG))
 
 def polite_request_sen(verb, noun=None):
 	jap = ''
@@ -45,11 +74,24 @@ def polite_request_sen(verb, noun=None):
 	return jap, eng
 
 def polite_request_sen_examples():
-	print(polite_request_sen('eat'))
-	print(polite_request_sen('watch', 'film'))
-	print(polite_request_sen('turn off', 'light'))
-	print(polite_request_sen('turn on', 'TV'))
-	print(polite_request_sen('wait'))
+	print_and_say(polite_request_sen('eat'))
+	print_and_say(polite_request_sen('watch', 'film'))
+	print_and_say(polite_request_sen('turn off', 'light'))
+	print_and_say(polite_request_sen('turn on', 'TV'))	
+	print_and_say(polite_request_sen('wait'))
+	
+def say_numbers():
+	for i in range(1, 11):
+		logn = random.uniform(1.8, 4.5)
+		n = int(10**logn)
+		say_jap_wait(f'{str(i)} ばん', 2)
+		for _ in range(2):
+			say_jap_wait(str(n), 5)
+			
+		say_eng_wait(f'That was {str(n)}.', 1)
+		print(i, ':', n)
+		say_jap_wait(str(n), 3)
+	say_jap_wait('おわりました')
 
 
 
@@ -58,9 +100,12 @@ if __name__ == '__main__':
 	import doctest
 
 	doctest.testmod()
+	
+	print(speech.get_synthesis_languages())
 
-	basic_sen_examples()
+	# basic_sen_examples()
 	print()
-	polite_request_sen_examples()
-
+	# polite_request_sen_examples()
+	
+	say_numbers()
 
