ffa9e841c80d7c11399b2d0f2ea0ef2dd0f0ddae
Ian Poole <ipoole@icloud.com>
2020-11-12 20:11:50 +0000

    Added times_quiz()

--- numbers_quiz.py
+++ numbers_quiz.py
@@ -16,5 +16,22 @@ def numbers_quiz(num_questions=10, repeats=2, delay=4):
     say_jap_wait('おわりました')
 
 
+def times_quiz(num_questions=10, repeats=2, delay=4):
+    for i in range(1, num_questions + 1):
+        hrs = random.randint(0, 12)
+        mins = random.choice([5, 15, 15, 30, 30, 30, 45, 45, 55, 40, 20, 10])
+        am_or_pm = random.choice(['am', 'pm'])
+        say_jap_wait(f'{str(i)} ばん', 1)
+        time_eng = f"{hrs}:{mins} {am_or_pm}"
+        for _ in range(repeats):
+            say_jap_wait(time_eng, delay)   # Japanese speech cleverly interprets English times!
+        print(i, ':', time_eng)
+        say_eng_wait(f'That was {time_eng}', 1)
+        say_jap_wait(time_eng, 2)
+    say_jap_wait('おわりました')
+
+
 if __name__ == "__main__":
     numbers_quiz(num_questions=5, repeats=2, delay=4)
+    say_jap_wait('なんじですか', 2)
+    times_quiz(num_questions=5, repeats=2, delay=4)
--- test_numbers_quiz.py
+++ test_numbers_quiz.py
@@ -6,6 +6,9 @@ class TestNumbersQuiz(unittest.TestCase):
     def test_numbers_quiz(self):
         numbers_quiz.numbers_quiz(num_questions=2, repeats=1, delay=1)
 
+    def test_times_quiz(self):
+        numbers_quiz.times_quiz(num_questions=2, repeats=1, delay=1)
+
 
 if __name__ == '__main__':
     unittest.main()
